9d0b1b675d12d55033bbe3e830ee93f0c4936d73
CarDloDev <charles4586@gmail.com>
2025-01-10 13:47:13 -0500

    Actulizacion de panel API DOCS -rev1

--- principal/settings.py
+++ principal/settings.py
@@ -220,7 +220,7 @@ CSRF_TRUSTED_ORIGINS = ['http://localhost', 'https://web-production-79a9.up.rail
 # settings.py
 SPECTACULAR_SETTINGS = {
     'TITLE': 'API Solar Management',
-    'DESCRIPTION': 'Panel de documentación con Sidebar',
+    'DESCRIPTION': 'Panel de documentación de la API Solar Management',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,  
     'SWAGGER_UI_SETTINGS': {
