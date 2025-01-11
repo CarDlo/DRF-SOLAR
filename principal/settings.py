f5a15fda0cd35b7452addd4a9856226632ab96b4
CarDloDev <charles4586@gmail.com>
2025-01-10 19:22:25 -0500

    Actulizacion del host DO

--- principal/settings.py
+++ principal/settings.py
@@ -48,7 +48,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','web-production-79a9.up.railway.app']
+ALLOWED_HOSTS = ['localhost','web-production-79a9.up.railway.app','164.92.71.165']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
