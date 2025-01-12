e1b5dce4432c99406978749a472c4b3bb51d8ab4
CarDloDev <charles4586@gmail.com>
2025-01-11 20:11:39 -0500

    Prueba - Funcional V8 /Coreapi Debug off

--- principal/settings.py
+++ principal/settings.py
@@ -46,7 +46,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','web-production-79a9.up.railway.app','164.92.71.165']
 
