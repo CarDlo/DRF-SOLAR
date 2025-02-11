e61dfd9170d0979fcd7fb24c23462ce3a1264ca5
CarDloDev <charles4586@gmail.com>
2025-02-11 07:00:41 -0500

    Allow any user access by updating default permission classes in settings

--- principal/settings.py
+++ principal/settings.py
@@ -196,6 +196,7 @@ REST_FRAMEWORK = {
     #SIMPLEJWT
     'DEFAULT_PERMISSION_CLASSES': (
         #'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         #'rest_framework_simplejwt.authentication.JWTAuthentication',
